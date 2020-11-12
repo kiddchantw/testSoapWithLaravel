b063ac937f58f30893efd6c8d1a8c7b72dbe53c8
K88 <kiddchantw@gmail.com>
2020-11-12 09:59:27 +0800

    feat curl soap/xml  m1.1 ok

--- app/Http/Controllers/SOAPcontroller.php
+++ app/Http/Controllers/SOAPcontroller.php
@@ -6,32 +6,34 @@ use Illuminate\Http\Request;
 
 class SOAPcontroller extends Controller
 {
-    
+
     public function method2()
     {
-        $client = new http\Client;
-$request = new http\Client\Request;
-$request->setRequestUrl('https://www.dataaccess.com/webservicesserver/NumberConversion.wso');
-$request->setRequestMethod('POST');
-$body = new http\Message\Body;
-$body->append('<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    <NumberToWords xmlns="http://www.dataaccess.com/webservicesserver/">
-      <ubiNum>500</ubiNum>
-    </NumberToWords>
-  </soap:Body>
-</soap:Envelope>');
-$request->setBody($body);
-$request->setOptions(array());
-$request->setHeaders(array(
-  'Content-Type' => 'text/xml; charset=utf-8'
-));
-$client->enqueue($request)->send();
-$response = $client->getResponse();
-echo $response->getBody();
+        //m1的改寫。另一個專案測試ok
+        $xml_data = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n
+    <soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n
+        <soap:Body>\n
+            <NumberToDollars xmlns=\"http://www.dataaccess.com/webservicesserver/\">\n
+                <dNum>500</dNum>\n
+            </NumberToDollars>\n
+     </soap:Body>\n</soap:Envelope>";
+        $URL = "https://www.dataaccess.com/webservicesserver/NumberConversion.wso";
+
+        $ch = curl_init($URL);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_ENCODING, '');
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "$xml_data");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: text/xml; charset=utf-8"));
+        $output = curl_exec($ch);
+        curl_close($ch);
+        print_r($output);
     }
-    
+
     //
     public function method_curl()
     {
@@ -57,9 +59,9 @@ echo $response->getBody();
                 "Content-Type: text/xml; charset=utf-8"
             ),
         ));
-    
+
         $response = curl_exec($curl);
-    
+
         curl_close($curl);
         echo $response;
     }
